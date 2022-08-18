908d6e5159789228ccc5d8cc5a61c9b3aa8f152d
ArisPagonopoulos <37390050+ArisPagonopoulos@users.noreply.github.com>
2022-08-18 03:08:10 +0300

    some of the examples on usage function didn't need the =

--- chapter02/bhnet.py
+++ chapter02/bhnet.py
@@ -174,8 +174,8 @@ def usage():
     print()
     print("Examples: ")
     print("bhpnet.py -t 192.168.0.1 -p 5555 -l -c")
-    print("bhpnet.py -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe")
-    print("bhpnet.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\"")
+    print("bhpnet.py -t 192.168.0.1 -p 5555 -l -u c:\\target.exe")
+    print("bhpnet.py -t 192.168.0.1 -p 5555 -l -e \"cat /etc/passwd\"")
     print("echo 'ABCDEFGHI' | ./bhpnet.py -t 192.168.11.12 -p 135")
     sys.exit(0)
 
